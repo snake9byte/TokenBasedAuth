5068979f7caa723451b51cc755e1e4ab859453fb
snake9byte <snake9byte@tutamail.com>
2024-02-16 20:29:52 +0200

    Optimize database queries

---
+++ js/demo.js
@@ -0,0 +1,152 @@
+
+//JSONData[page][row] = {id,time,user,realname,email,pho};
+var JSONData = [[{id:"001",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}],
+             
+             [{id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}],
+             
+             [{id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}],
+             
+             [{id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}],
+             
+             [{id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}],
+             
+             [{id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}],
+             
+             [{id:"007",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}],
+             
+             [{id:"008",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}],
+             
+             [{id:"009",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}],
+             
+             [{id:"010",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"002",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"003",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"004",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"005",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"},
+              {id:"006",time:"2015-07-05 04:20:03",user:"username",realname:"real name",email:"eamil@zui.sexy",pho:"18888888888"}]];
+
+// 当前也与总页数
+var curPage = 1;
+var pageSum = JSONData.length;
+
+$(function() {
+    
+    // nav click
+    $("#mynav li").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+    
+    // datatable初始化
+    $('table.datatable').datatable({
+        checkable: true,
+        sortable: true,
+        checkedClass: "active",
+    });
+    
+    //    $("[data-toggle=pager]").pager();
+
+    
+    $('.pager li:not(.disabled)').click(function() {
+        var cur = $(this).closest('.pager').find('li.active');  // 获取当前激活按钮
+        cur.removeClass('active');                              // remove激活
+        if ($(this).hasClass('previous')) {     // 当前点击的是上一页                
+            curPage = --curPage <= 1 ? 1 : curPage; // 更新页码
+            if(curPage==1){
+                $(this).next().addClass('active');      
+            }
+            else{
+                cur.prev().addClass('active');
+            }
+        }
+        else if ($(this).hasClass('next')) {    // 当前点击的是下一页
+            curPage = ++curPage >= pageSum ? pageSum : curPage;
+            if(curPage==pageSum){
+                $(this).prev().addClass('active');
+            }
+            else{
+                cur.next().addClass('active');
+            }
+        } else {                                // 当前点击的为页码
+            curPage = $(this).find('a').html();
+            $(this).addClass('active');
+        }
+        
+        // 判断如果当前是第一页，给previous加上disabled
+        if(curPage==1){
+            $(this).closest('.pager').find('.previous').addClass('disabled');
+        }
+        else{
+            $(this).closest('.pager').find('.previous').removeClass('disabled');
+        }
+        
+        // 判断如果当前是最后一页，给next加上disabled
+        if(curPage==pageSum){
+            $(this).closest('.pager').find('.next').addClass('disabled');
+        }
+        else{
+            $(this).closest('.pager').find('.next').removeClass('disabled');
+        }
+        
+        // htmlcode 为更新的表格
+        
+        var htmlcode = '<table class="table datatable table-striped "><thead><tr><th data-type="number" width="80px">#</th><th data-type="date" width="160px">时间</th><th>用户名</th><th>真实姓名</th><th>邮箱</th><th>电话</th><th class="sort-disabled" width="80px">操作</th></tr></thead><tbody>';
+        for (var i = 0; i < JSONData[curPage - 1].length; ++i) {
+            htmlcode += '<tr><td>' + JSONData[curPage - 1][i].id + '</td><td>' + JSONData[curPage - 1][i].time + '</td><td>' + JSONData[curPage - 1][i].user + '</td><td>' + JSONData[curPage - 1][i].realname + '</td> <td>' + JSONData[curPage - 1][i].email + '</td><td>' + JSONData[curPage - 1][i].pho + '</td><td><a href="#"><i class="icon icon-ok-sign"></i></a>&nbsp;&nbsp;<a href="#" class="text-danger"><i class="icon icon-trash"></i></a></td></tr>';
+        }
+        htmlcode += '</tbody></table>';
+        
+        // 将#mycontent的html代码更新，并且初始化表格
+        $('#mycontent').html(htmlcode);
+        $('table.datatable').datatable({
+            checkable: true,
+            sortable: true,
+            checkedClass: "active",
+        });
+    });
+});
